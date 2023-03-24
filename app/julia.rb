9088a895d014742213d69edb1f24e91edfa6f94b
osawa-koki <github@osawa.dev>
2023-03-24 21:44:20 +0900

    julia

--- app/julia.rb
+++ app/julia.rb
@@ -16,29 +16,35 @@ y_min = julia_config['y_min'].to_f
 y_max = julia_config['y_max'].to_f
 cx = julia_config['cx'].to_f
 cy = julia_config['cy'].to_f
-max_iter = julia_config['max_iter'].to_i
-internal_color = julia_config['internal_color']
-external_color = julia_config['external_color']
+threshold = julia_config['threshold'].to_f
+max_iterations = julia_config['max_iterations'].to_i
+color = julia_config['color'].to_i
+background_color = julia_config['background_color']
 output_file = julia_config['output_file']
 
 image = Magick::Image.new(width, height)
 
+x_delta = (x_max - x_min) / width
+y_delta = (y_max - y_min) / height
+c = { x: cx, y: cy }
+
 # ジュリア集合の描画
-for y in 0..height-1 do
-  for x in 0..width-1 do
-    zx = x_min + (x_max - x_min) * x / width
-    zy = y_min + (y_max - y_min) * y / height
+(0..width-1).each do |x|
+  (0..height-1).each do |y|
+    z = { x: x_min + x * x_delta, y: y_min + y * y_delta }
     i = 0
-    while zx * zx + zy * zy < 4 && i < max_iter do
-      tmp = zx * zx - zy * zy + cx
-      zy = 2 * zx * zy + cy
-      zx = tmp
+    while i < max_iterations
+      z2 = { x: z[:x] * z[:x] - z[:y] * z[:y], y: 2 * z[:x] * z[:y] }
+      z[:x] = z2[:x] + c[:x]
+      z[:y] = z2[:y] + c[:y]
+      break if z[:x] * z[:x] + z[:y] * z[:y] > threshold
       i += 1
     end
-    if i == max_iter then
-      image.pixel_color(x, y, external_color)
+    if i == max_iterations
+      image.pixel_color(x, y, background_color)
     else
-      image.pixel_color(x, y, internal_color)
+      color_hsl = "hsl(#{color + i * 360 / max_iterations}, 100%, 50%)"
+      image.pixel_color(x, y, Magick::Pixel.from_color(color_hsl))
     end
   end
 end
