0b5e84ccb240964d3b61a94fcb74016ae99f03eb
osawa-koki <github@osawa.dev>
2023-03-25 13:36:25 +0900

    小さな修正。

--- app/sierpinski_triangle.rb
+++ app/sierpinski_triangle.rb
@@ -12,6 +12,7 @@ height = global_config['height'].to_i
 output_dir = global_config['output_dir']
 
 $color = sierpinski_triangle_config['color']
+$filling_color = sierpinski_triangle_config['filling_color']
 triagle_size = sierpinski_triangle_config['triagle_size'].to_i
 $max_iterations = sierpinski_triangle_config['max_iterations'].to_i
 output_file = sierpinski_triangle_config['output_file']
@@ -24,7 +25,7 @@ def draw_triangle(x, y, size)
   p2_x = x + size
   p2_y = y
   draw = Magick::Draw.new
-  draw.fill("white")
+  draw.fill($filling_color)
   draw.polygon(x, y, p1_x, p1_y, p2_x, p2_y)
   draw.draw($image)
 end
@@ -66,7 +67,7 @@ def rec_fx(x, y, size, n)
 end
 
 size = width * triagle_size / 100
-start = (width - (Math.sqrt(3) * width * triagle_size / 100 / 2)) / 2
+start = (height - (Math.sqrt(3) * height * triagle_size / 100 / 2)) / 2
 
 p1_x = Math.cos(240 * BigDecimal(Math::PI.to_s) / 180) * size / 2 + width / 2
 p1_y = start - Math.sin(240 * BigDecimal(Math::PI.to_s) / 180) * size / 2
@@ -81,7 +82,7 @@ draw.polygon(width / 2, start, _p2_x, _p2_y, _p2_x + size, _p2_y)
 draw.draw($image)
 
 draw = Magick::Draw.new
-draw.fill("white")
+draw.fill($filling_color)
 draw.polygon(p1_x, p1_y, p2_x, p2_y, p1_x + size / 2, p1_y)
 draw.draw($image)
 
